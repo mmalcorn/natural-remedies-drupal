54dbb5ef92de32b84f4d5913db84a8d20dca0790
mmalcorn <alcornmeredith@gmail.com>
2016-12-21 21:51:53 -0500

    add more content for remedies and ailments.  create yoga posture content type.  create views for remedies and ailments

---
+++ sites/default/files/php/twig/4072e7b8_views-exposed-form.html.twig_8d7f1c157ee5db15d9aafd61d025ed1f73317fedb58134881c1678e4148e6d7c/5e13d2dc930ffa4e40e5fc3e66489814ee2232d63cd1852dfee048708d87d10e.php
@@ -0,0 +1,97 @@
+<?php
+
+/* core/themes/classy/templates/views/views-exposed-form.html.twig */
+class __TwigTemplate_51b6ee4315410ab7b1e6dc4a21776fa460dab26b5ffafe8ec62213d5b0c6f98a extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 12);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 12
+        if ( !twig_test_empty((isset($context["q"]) ? $context["q"] : null))) {
+            // line 13
+            echo "  ";
+            // line 17
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["q"]) ? $context["q"] : null), "html", null, true));
+            echo "
+";
+        }
+        // line 19
+        echo "<div class=\"form--inline clearfix\">
+  ";
+        // line 20
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["form"]) ? $context["form"] : null), "html", null, true));
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/views/views-exposed-form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 20,  52 => 19,  47 => 17,  45 => 13,  43 => 12,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a views exposed form.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - form: A render element representing the form.*/
+/*  **/
+/*  * @see template_preprocess_views_exposed_form()*/
+/*  *//* */
+/* #}*/
+/* {% if q is not empty %}*/
+/*   {#*/
+/*     This ensures that, if clean URLs are off, the 'q' is added first,*/
+/*     as a hidden form element, so that it shows up first in the POST URL.*/
+/*   #}*/
+/* {{ q }}*/
+/* {% endif %}*/
+/* <div class="form--inline clearfix">*/
+/*   {{ form }}*/
+/* </div>*/
+/* */
