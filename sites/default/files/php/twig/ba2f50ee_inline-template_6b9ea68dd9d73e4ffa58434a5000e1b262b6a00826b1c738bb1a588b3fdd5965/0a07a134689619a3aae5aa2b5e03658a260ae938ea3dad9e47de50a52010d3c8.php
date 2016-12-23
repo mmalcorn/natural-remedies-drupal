406b0a6b80f3814ca8c86454d74ccb5e3669c3fe
Meredith Alcorn <meredithalcorn@Merediths-MacBook-Pro.local>
2016-12-22 16:23:16 -0800

    create a view to display the description of an ailment, and a second page on that same view to display the remedies

---
+++ sites/default/files/php/twig/ba2f50ee_inline-template_6b9ea68dd9d73e4ffa58434a5000e1b262b6a00826b1c738bb1a588b3fdd5965/0a07a134689619a3aae5aa2b5e03658a260ae938ea3dad9e47de50a52010d3c8.php
@@ -0,0 +1,62 @@
+<?php
+
+/* {# inline_template_start #}ailment/{{ nid }} */
+class __TwigTemplate_12d0aec03a53f1ac267e15cb64471bbb435f960756a3b73176b7d65e5e5c3917 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "ailment/";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["nid"]) ? $context["nid"] : null), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}ailment/{{ nid }}";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+}
+/* {# inline_template_start #}ailment/{{ nid }}*/
